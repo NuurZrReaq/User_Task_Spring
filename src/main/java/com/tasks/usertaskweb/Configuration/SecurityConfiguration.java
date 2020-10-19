44b84405b1a38d3203ec928a0a7fa8c31c77706e
NuurZrReaq <nuur.zreaq@gmail.com>
2020-10-19 19:28:37 +0300

    hi

--- src/main/java/com/tasks/usertaskweb/Configuration/SecurityConfiguration.java
+++ src/main/java/com/tasks/usertaskweb/Configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package com.tasks.usertaskweb.Configuration;
 import Services.JwtUtil;
 import Services.MyUserDetailsService;
 
+import com.tasks.usertaskweb.filters.JwtFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,29 +12,37 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
 @Configuration
 @EnableWebSecurity
+
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
 
     @Autowired
     MyUserDetailsService userDetailsService;
 
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/authenticate").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                ;
+
+
+
     }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService);
     }
+
 
     @Bean
     public PasswordEncoder getPasswordEncoder (){
@@ -51,10 +60,12 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter{
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
     }
+
     @Bean
     public JwtUtil jwtUtilBean() throws Exception{
         return new JwtUtil();
     }
+
 
 
 
